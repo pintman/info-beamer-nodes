20a4ac79463916b14fa4c604c8d42b45caf11b55
Marco Bakera <Marco@Bakera.de>
2016-01-24 15:06:44 +0100

    added logo

--- itf13b-messe/node.lua
+++ itf13b-messe/node.lua
@@ -1,6 +1,8 @@
 
 gl.setup(800, 600)
 
+logo = resource.load_image("logo-tbs1.png")
+
 function node.render()
     --gl.clear(0, 1, 0, 1) -- green
     gl.clear(0, 0, 0, 1) -- black
@@ -13,7 +15,10 @@ function node.render()
     clock = resource.render_child("analogclock")
     clock:draw(WIDTH-100, 0, WIDTH, 100)
 
+    -- draw logo
+    util.draw_correct(logo, WIDTH-90, HEIGHT-150, WIDTH, HEIGHT-20)
+
     -- dispose temp images to save gpu ram
     flap.dispose()
-    clock.dispise()
+    clock.dispose()
 end
