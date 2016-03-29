da7df29a97882b13c8697afff91d1eef026955f0
Marco Bakera <Marco@Bakera.de>
2016-03-29 08:38:22 +0200

    added information about projects

--- ita13-messe/run-display.sh
+++ ita13-messe/run-display.sh
@@ -1,13 +1,18 @@
 #/bin/sh
 
+# wait time (in seconds) between slides
+TIME=30
+
 # set clock
 python analogclock/set_clock.py
 
 while true;
-do
-  nc localhost 4444 < 1_willkommen.txt;
-  sleep 30;
-  nc localhost 4444 < 2_projekte.txt;
-  sleep 30;
+do  
+  for f in 1_willkommen.txt Projekte-*;
+  do
+	nc localhost 4444 < $f;
+	cat $f
+	sleep $TIME;
+  done
 done
 
